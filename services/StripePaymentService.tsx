2b3d850c4d39ffb107c7b4ce51e6a44d8505ba43
Alisha-Reddy <kondapualishareddy@gmail.com>
2025-01-04 22:27:49 +0530

    .

---
+++ services/StripePaymentService.tsx
@@ -0,0 +1,35 @@
+export const createCheckoutSession = async (amount: number, paymentPeriod: string) => {
+  try {
+    const response = await fetch('http://localhost:3001/stripe/create-checkout-session', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ amount, paymentPeriod }),
+    });
+    const checkoutUrl = await response.text();
+    return checkoutUrl; // This should return the Stripe checkout URL
+  } catch (error) {
+    console.error('Error creating Stripe Checkout session:', error);
+    throw error;
+  }
+};
+
+export const createPaymentWithConfiguration = async (amount: number, paymentPeriod: string, paymentMethodConfigurationId: string) => {
+  try {
+    const response = await fetch('http://localhost:3001/stripe/create-payment-intent', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ amount, paymentPeriod, paymentMethodConfigurationId }),
+    });
+
+    const intentData = await response.text();
+    console.log("1",intentData)
+    return intentData; // This should return the PaymentIntent data
+  } catch (error) {
+    console.error('Error creating Stripe Payment Intent:', error);
+    throw error;
+  }
+};
